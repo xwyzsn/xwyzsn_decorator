f4d52b5ff127c432acce099a130573ae282d354d
xwyzsn <xwyzsn@gmail.com>
2024-07-27 11:36:27 +0800

    conflict with exsiting package

--- decorator/__init__.py
+++
@@ -1,3 +0,0 @@
-from decorator.time.timer import timer
-from decorator.pytorch_search.search import search_with_cuda
-from decorator.retry.retry import retry
---
+++ x_decorator/__init__.py
@@ -0,0 +1,3 @@
+from x_decorator.retry import retry
+from x_decorator.search import search_with_cuda
+from x_decorator.timer import timer
